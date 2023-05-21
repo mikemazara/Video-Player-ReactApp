ea36b8895f15597db3ab71a2758066d9bd0c8406
mikemazara <mike.mazara@gmail.com>
2023-05-20 21:33:18 -0700

    mobile styling comments

--- Documents/workspace/Mike-Mazara-Brainflix/mike-mazara-brainflix/src/components/comments.js
+++ Documents/workspace/Mike-Mazara-Brainflix/mike-mazara-brainflix/src/components/comments.js
@@ -1,70 +1,69 @@
+import "./comments.scss";
+import avatar from "../assets/images/mohan-muruge.jpg";
+import commentIcon from "../assets/icons/add_comment.svg";
 
-import './comments.scss';
-import avatar from '../assets/images/mohan-muruge.jpg'
+function CommentItem({ comment: { id, name, timestamp, comment } }) {
+  let date = new Date(timestamp);
+  let mmDdYyyy = date.toLocaleDateString();
 
-
-
-
-
-// make function to apply to comment button to add comment to comments array
-
-    
-function CommentItem({comment: {id, name, timestamp, comment}}){
-    let date = new Date(timestamp);
-    let mmDdYyyy = date.toLocaleDateString();
-    
-    return (
-        <div className="comments__list-comment" key={id}>
-            <img className="comments__list-comment-image" src='#' alt="User avatar" />
-            <div className="comments__list-comment-details">
-                <div className="comments__list-comment-details-top">
-                    <h3 className="comments__list-comment-details-top-name">{name}</h3>
-                    <h4 className="comments__list-comment-details-top-date">{mmDdYyyy}</h4>
-                </div>
-                <p className="comments__list-comment-details-comment">{comment}</p>
-            </div>
+  return (
+    <div className="comments__comment" key={id}>
+      <div className="comments__list-avatar-container">
+        <img className="comments__list-avatar" src="#" alt="User avatar" />
+      </div>
+      <div className="comments__list-comment">
+        <div className="comments__top">
+          <h3 className="comments__name">{name}</h3>
+          <p className="comments__date">{mmDdYyyy}</p>
         </div>
-    )
-
+        <p className="comments__paragraph">{comment}</p>
+      </div>
+    </div>
+  );
 }
 
-function CommentsList({comments}){
-    return (
+function CommentsList({ comments }) {
+  return (
     <div className="comments__list">
-    {comments.map(comment => <CommentItem key={comment.id} comment={comment}/>)}
-</div>)
-
+      {comments.map((comment) => (
+        <CommentItem key={comment.id} comment={comment} />
+      ))}
+    </div>
+  );
 }
-function CommentsContainner({comments}) {
-
-
-
-    return (
-        <section className="comments">
-            <h2 className="comments__count">{comments.length} Comments</h2>
-            <div className="comments__form">
-                <div className='comments__avatar-container'>
-                    <img className="comments__avatar" src={avatar} alt="User avatar" />
-                </div>
-                <form className="comments__form-inputs">
-                    <label className="comments__form-inputs-label" htmlFor="comment">JOIN THE CONVERSATION</label>
-                    <textarea className="comments__form-inputs-textarea" name="comment" id="comment" cols="30" rows="10" placeholder="Write comment here"></textarea>
-                    <button className="comments__form-inputs-button" type="submit" >COMMENT</button>
-                </form>
-            </div>
-            <CommentsList comments={comments}/>
-        </section>
-
-    )
+function CommentsContainner({ comments }) {
+  return (
+    <section className="comments">
+      <h2 className="comments__count">{comments.length} Comments</h2>
+      <div className="comments__form">
+        <div className="comments__form-avatar-container">
+          <img
+            className="comments__form-avatar"
+            src={avatar}
+            alt="User avatar"
+          />
+        </div>
+        <form className="comments__input">
+          <label className="comments__input-label" htmlFor="comment">
+            JOIN THE CONVERSATION
+          </label>
+          <textarea
+            className="comments__input-text"
+            name="comment"
+            id="comment"
+            cols="30"
+            rows="10"
+            placeholder="Add a new comment"
+          ></textarea>
+          <img className="comments__input-icon" src={commentIcon}></img>
+          <button className="comments__input-button" type="submit">
+            COMMENT
+          </button>
+        </form>
+      </div>
+      <CommentsList comments={comments} />
+    </section>
+  );
 }
-
-
-
-
-
-
-
 
 export default CommentsContainner;
-
-    
