9478bc9ef6af9358e3144f64f4a32de0aa2ae383
mikemazara <mike.mazara@gmail.com>
2023-05-25 22:19:52 -0700

    get api working and start routes

--- src/App.js
+++ src/App.js
@@ -1,44 +1,84 @@
 import "./App.scss";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Header from "./components/header/header.js";
 import Hero from "./components/hero/hero.js";
-import videosLite from "./assets/data/videos.json";
-import videos from "./assets/data/video-details.json";
 import Comments from "./components/comment/comments";
 import NextVideo from "./components/next-videos/next-video";
-import { BrowserRouter, Routes, Route, Next } from "react-router-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Next,
+  useParams,
+  Navigate,
+} from "react-router-dom";
 import axios from "axios";
 
 const apiKey = "7201c760-19ae-48b7-8316-ea927c367d11";
 
-const fetchVideos = async () => {
-  const response = await axios.get(
-    `https://project-2-api.herokuapp.com/videos?api_key=7201c760-19ae-48b7-8316-ea927c367d11`
-  );
-  return response.data;
-};
-
-function fetchMainVideoFile(id) {
-  const thisVideo = videos.find((obj) => obj.id === id);
-  return thisVideo;
+async function fetchMainVideoFile(id) {
+  try {
+    const response = await axios.get(
+      `https://project-2-api.herokuapp.com/videos/${id}?api_key=${apiKey}`
+    );
+    const video = response.data;
+    return video;
+  } catch (error) {
+    console.error(error);
+    return {};
+  }
 }
 
-function fetchRelatedVideos(id) {
-  const allOtherVideos = videosLite.filter((obj) => obj.id !== id);
-  return allOtherVideos;
+async function fetchRelatedVideos(id) {
+  try {
+    const response = await axios.get(
+      `https://project-2-api.herokuapp.com/videos?api_key=${apiKey}`
+    );
+    const videosLite = response.data;
+    const allOtherVideos = videosLite.filter((obj) => obj.id !== id);
+    return allOtherVideos;
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
 }
 
 function App() {
   const [thisVideoId, setThisVideoId] = useState(
     "84e96018-4022-434e-80bf-000ce4cd12b8"
   );
-  const thisVideo = fetchMainVideoFile(thisVideoId);
-  const allOtherVideos = fetchRelatedVideos(thisVideoId);
+  const [thisVideo, setThisVideo] = useState(null);
+  const [allOtherVideos, setAllOtherVideos] = useState([]);
+
+  useEffect(() => {
+    fetchRelatedVideos(thisVideoId)
+      .then((videos) => setAllOtherVideos(videos))
+      .catch((error) => console.error(error));
+  }, [thisVideoId]);
+
+  useEffect(() => {
+    fetchMainVideoFile(thisVideoId)
+      .then((video) => setThisVideo(video))
+      .catch((error) => console.error(error));
+  }, [thisVideoId]);
+
+  if (!thisVideo) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <div className="App">
       <Header />
-      <Hero videoDetails={thisVideo} />
+      {/* <Hero videoDetails={thisVideo} />
+      <Comments comments={thisVideo.comments} />
+      <NextVideo videos={allOtherVideos} setThisVideoId={setThisVideoId} /> */}
+      {/* refactor return statement to include different routes for set new url everytime a new video is added to the hero section */}
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Hero videoDetails={thisVideo} />} />
+          <Route path="/:id" element={<Hero videoDetails={thisVideo} />} />
+        </Routes>
+      </BrowserRouter>
       <Comments comments={thisVideo.comments} />
       <NextVideo videos={allOtherVideos} setThisVideoId={setThisVideoId} />
     </div>
